2431377089f6262f8d647ba14a7d34d5b0d17287
Xming-Z <163409353+Xming-Z@users.noreply.github.com>
2024-09-12 13:29:04 +0800

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,40 +1,37 @@
 import onnx
 
 
-onnx.utils.extract_model('/data/Newdisk/zhaoxiaoming/steal/model_split_encryption/onnx/Military/Alexnet_Military.onnx',
-                         '/data/Newdisk/zhaoxiaoming/steal/model_split_encryption/splited_model/alexnet3.onnx',
+onnx.utils.extract_model('Alexnet_Military.onnx',
+                         'splited_model/alexnet3.onnx',
                          ['40'], ['output_data'])
 #i-14 14-19 19-o
 
-#onnx.utils.extract_model('/data/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/dp_onnx/imagenet_100/vgg16_imagenet_100.onnx',
-#                         '/data/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/dp_split_model/imagenet_100/vgg16/vgg16_2.onnx',
+#onnx.utils.extract_model('vgg16_imagenet_100.onnx',
+#                         '/vgg16/vgg16_2.onnx',
 #                        ['75'], ['output_data'])
 #i-25 25-39 39-o
 
-#onnx.utils.extract_model('/home/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/onnx/Military/VGG16_imagenet_100-299.onnx',
-#                          '/home/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/splited_model/Military/vgg16/vgg16_1.onnx',
+#onnx.utils.extract_model('VGG16_imagenet_100-299.onnx',
+#                          'vgg16/vgg16_1.onnx',
 #                         ['input_data'], ['75'])
 #i-39 39-54 54-o
 
-#onnx.utils.extract_model('/home/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/onnx/Military/resnet50_Military.onnx',
-#                          '/home/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/splited_model/Military/resnet50/resnet50_model3.onnx',
+#onnx.utils.extract_model('resnet50_Military.onnx',
+#                          '/resnet50/resnet50_model3.onnx',
 #                          ['439'], ['output_data'])
 #i-355 355-439 439-o
 
-#onnx.utils.extract_model('/home/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/onnx/Military/GoogleNet_Military.onnx',
-#                          '/home/Newdisk/zhaoxiaoming/project_model_stealing/model_split_encryption/splited_model/Military/googlenet/googlenet2.onnx',
+#onnx.utils.extract_model('GoogleNet_Military.onnx',
+#                          '/googlenet/googlenet2.onnx',
 #                          ['559'], ['output_data'])
 
 #i-417 417-518 518-o
 
-#onnx.utils.extract_model('/data/Newdisk/zhaoxiaoming/object_detection/model/ssd_model/dp_onnx/ssd.onnx',
-#                          '/data/Newdisk/zhaoxiaoming/object_detection/dp_split_model/ssd/ssd2.onnx',
+#onnx.utils.extract_model('ssd.onnx',
+#                          '/split_model/ssd/ssd2.onnx',
 #                          ['93'],['output','277'])
 						  
-#onnx.utils.extract_model('/data/Newdisk/zhaoxiaoming/object_detection/model/frcnn_model/dp_onnx/frcnn.onnx',
-#                          '/data/Newdisk/zhaoxiaoming/object_detection/dp_split_model/faster_rcnn/faster_rcnn1.onnx',
+#onnx.utils.extract_model('frcnn.onnx',
+#                          '/split_model/faster_rcnn/faster_rcnn1.onnx',
 #                         ['images'],['948','1093','474'])
 						  
-#onnx.utils.extract_model('/data/Newdisk/zhaoxiaoming/object_detection/model/frcnn_model/dp_onnx/frcnn.onnx',
-#                          '/data/Newdisk/zhaoxiaoming/object_detection/dp_split_model/faster_rcnn/faster_rcnn2.onnx',
-#                         ['1093','474'],['output','1122'])
